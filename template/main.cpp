3952ae164ef97dc59e09b63b1e19378d0692a1e0
jayesh <keshri23.jayesh@gmail.com>
2020-06-21 09:02:35 +0530

    sample template

---
+++ template/main.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, '0', sizeof(x))
+#define sortall(x) sort(all(x))
+#define fo(i,Q) for(i=0;i<Q;i++)
+#define fo1(j,N) for(j=1;j<=N;j++)
+
+typedef long long int ll;
+typedef vector<int> vec;
+typedef pair<int, int>	pi;
+typedef pair<ll, ll>	pl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+
+int main()
+{
+    ll T,N,find,counter=0,i;
+    cin>>T;
+	while(T--)
+	{
+        cin>>N;
+	}
+	return 0;
+}
